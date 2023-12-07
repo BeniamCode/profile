ef9bcb12780e98353752a74d2aadb77595284b36
beniam <dbeniam@gmail.com>
2023-12-07 09:26:01 +0200

    update site
    update

---
+++ src/asset/_next/static/chunks/pages/index-1e8c55e603442271.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{9737:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(7991)}])},7991:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return e}});var t=_(6942),e=!0;u.default=t.default}},function(n){n.O(0,[927,335,464,158,774,888,179],function(){return n(n.s=9737)}),_N_E=n.O()}]);
